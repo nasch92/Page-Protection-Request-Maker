997925b0a09f3e71035ac742687823f79982c802
nasch92 <ignaciocr92@gmail.com>
2022-07-09 01:15:26 +0200

    Creates simple window when at the right page

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-more
 
 //Test
 console.log("Loading Page Protection Request Maker...");
+
+function createFormWindow() {
+    let Window = new Morebits.simpleWindow(620, 530);
+}
+
+if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
+    console.log("Special or non-existent page: PPRM will therefore not be loaded.");
+} else {
+    let portletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'example-button', 'Solicita que esta página sea protegida');
+    portletLink.onclick = createFormWindow;
+}
