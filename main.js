922540aababaf99966cda52204f7a70eb3c85ea0
nasch92 <ignaciocr92@gmail.com>
2022-07-09 00:52:46 +0200

    Adding morebits import

---
+++ main.js
@@ -0,0 +1,5 @@
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css');
+
+
+console.log("Loading Page Protection Request Maker...");
