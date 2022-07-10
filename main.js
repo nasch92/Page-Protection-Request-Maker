04fcb5a7f94bfb3812bc24654ba29ead7a4f7708
nasch92 <ignaciocr92@gmail.com>
2022-07-11 00:37:17 +0200

    [WIP] Added layout for the form

--- main.js
+++ main.js
@@ -4,14 +4,90 @@ mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-more
 //Test
 console.log("Loading Page Protection Request Maker...");
 
+let listProtectionOptions = [
+	{ code: "protección", name: "Solicitar protección", default: true },
+	{ code: "desprotección", name: "Solicitar desprotección" }
+]
+
+function getProtectionTypeOptions() {
+	let dropDownOptions = [];
+	for (let chosenType of listProtectionOptions) {
+		let option = { type: 'option', value: chosenType.code, label: chosenType.name, checked: chosenType.default };
+		dropDownOptions.push(option);
+	}
+	return dropDownOptions;
+}
+
+let listMotiveOptions = [
+	{ name: "Vandalismo" },
+	{ name: "SPAM" },
+	{ name: "Información falsa o especulativa" },
+	{ name: "Guerra de ediciones" },
+	{ name: "Otro" }
+]
+
+function getMotiveOptions() {
+	let dropDownOptions = [];
+	for (let chosenType of listMotiveOptions) {
+		let option = { type: 'option', label: chosenType.name, checked: chosenType.default };
+		dropDownOptions.push(option);
+	}
+	return dropDownOptions
+}
+
 function createFormWindow() {
-    let Window = new Morebits.simpleWindow(620, 530);
-    Window.display();
+	let Window = new Morebits.simpleWindow(620, 530);
+	Window.setTitle('Solicitar protección de la página');
+	Window.addFooterLink('Política de protección', 'Wikipedia:Política de protección');
+	let form = new Morebits.quickForm(submitMessage);
+	let radioField = form.append({
+		type: 'field',
+		label: 'Tipo:',
+	});
+
+	radioField.append({
+		type: 'radio',
+		name: 'protection',
+		list: getProtectionTypeOptions()
+	})
+
+	let textAreaAndReasonField = form.append({
+		type: 'field',
+		label: 'Opciones:',
+	});
+
+	textAreaAndReasonField.append({
+		type: 'select',
+		name: 'motive',
+		label: 'Selecciona el motivo:',
+		list: getMotiveOptions()
+	});
+
+	textAreaAndReasonField.append({
+		type: 'textarea',
+		name: 'reason',
+		label: 'Desarrolla la razón:',
+		tooltip: 'Puedes usar wikicódigo en tu descripción, tu firma se añadirá automáticamente.'
+	});
+	form.append({
+		type: 'submit',
+		label: 'Aceptar'
+	});
+
+	let result = form.render();
+	Window.setContent(result);
+	Window.display();
+}
+
+function submitMessage(e) {
+	let form = e.target;
+	let input = Morebits.quickForm.getInputData(form);
+	console.log(input.protection)
 }
 
 if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
-    console.log("Special or non-existent page: PPRM will therefore not be loaded.");
+	console.log("Special or non-existent page: PPRM will therefore not be loaded.");
 } else {
-    let portletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'example-button', 'Solicita que esta página sea protegida');
-    portletLink.onclick = createFormWindow;
+	let portletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'example-button', 'Solicita que esta página sea protegida');
+	portletLink.onclick = createFormWindow;
 }
