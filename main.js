19c3510bc8916898c479a38d0539679df124faed
nasch92 <ignaciocr92@gmail.com>
2022-07-14 20:10:03 +0200

    Added code allowing the script to show the protection status
    Wrote code that finds the protection level of the current page through the mw API, then adds it to the form through a `div` morebits element. Also switched the jQuery functionalities for Javascript ones doing the same job.

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-more
 //Test
 console.log("Loading Page Protection Request Maker...");
 
-let nominatedPageName = mw.config.get('wgPageName')
-let nominatedPageNameWithoutUnderscores = nominatedPageName.replaceAll('_', ' ')
+let pageName = mw.config.get('wgPageName')
+let pageNameWithoutUnderscores = pageName.replaceAll('_', ' ')
 
 let listProtectionOptions = [
 	{ code: "protección", name: "Solicitar protección", default: true },
@@ -38,6 +38,35 @@ function getMotiveOptions() {
 	return dropDownOptions
 }
 
+function protectionFromGetReply(data) {
+	let pages = data.query.pages;
+	for (p in pages) {
+		protectionLevel = pages[p].protection[0]?.level
+		switch (protectionLevel) {
+			case 'sysop':
+				return 'solo bibliotecarios';
+			case 'autoconfirmed':
+				return 'solo usuarios autoconfirmados';
+			default:
+				return 'sin protección';
+		}
+	}
+}
+
+function getProtectionStatus() {
+	let params = {
+		action: 'query',
+		prop: 'info',
+		inprop: 'protection',
+		titles: pageName,
+		format: 'json',
+	}
+	let apiPromise = new mw.Api().get(params);
+	let protectionPromise = apiPromise.then(protectionFromGetReply);
+
+	return protectionPromise;
+}
+
 function createStatusWindow() {
 	let Window = new Morebits.simpleWindow(400, 350);
 	Window.setTitle('Procesando acciones');
@@ -47,6 +76,7 @@ function createStatusWindow() {
 	Morebits.status.init(statusdiv);
 	Window.display();
 }
+
 
 function createFormWindow() {
 	let Window = new Morebits.simpleWindow(620, 530);
@@ -63,9 +93,20 @@ function createFormWindow() {
 		name: 'protection',
 		event:
 			function (e) {
-				$("select[name='motive']").prop('disabled', e.target.value !== "protección")
+				nameToModify = document.querySelector("select[name='motive']")
+				if (e.target.value !== "protección") {
+					nameToModify.setAttribute('disabled', "")
+				} else {
+					nameToModify.removeAttribute('disabled', "")
+				}
 			},
 		list: getProtectionTypeOptions()
+	})
+
+	form.append({
+		type: 'div',
+		name: 'currentProtection',
+		label: `Nivel actual de protección: `
 	})
 
 	let textAreaAndReasonField = form.append({
@@ -95,6 +136,11 @@ function createFormWindow() {
 	let result = form.render();
 	Window.setContent(result);
 	Window.display();
+
+	getProtectionStatus().then(function (protectionLevel) {
+		document.querySelector("div[name='currentProtection'] > span.quickformDescription")
+			.innerHTML = `Nivel actual de protección:<span style="color:green; font-weight: bold;"> ${protectionLevel} <span>`
+	})
 }
 
 function submitMessage(e) {
@@ -103,7 +149,7 @@ function submitMessage(e) {
 	if (input.reason === ``) {
 		alert("No se ha establecido un motivo.");
 	} else {
-		if (window.confirm(`¿Quieres solicitar la ${input.protection} del artículo ${nominatedPageNameWithoutUnderscores}?`)) {
+		if (window.confirm(`¿Quieres solicitar la ${input.protection} del artículo ${pageNameWithoutUnderscores}?`)) {
 			console.log("Posting message on the noticeboard...")
 			createStatusWindow()
 			new Morebits.status("Paso 1", `Solicitando la ${input.protection} de la página...`, "info");
